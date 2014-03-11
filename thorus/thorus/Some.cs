67ddee78a4b6a0f44564eb610e6004836893ee6b
mariano <mariano.hayon@southworks.net>
2014-03-11 16:59:21 -0300

    -main project

---
+++ thorus/thorus/Some.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thorus
+{
+    class Some
+    {
+        public Some()
+        {
+            var num = 0;
+            num = 5 + 5;
+        }
+    }
+}
