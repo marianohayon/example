779e5e2b9076866ee1325d6492f88a2ee3dff3d2
mariano <mariano.hayon@southworks.net>
2014-03-12 15:22:50 -0300

    -t

---
+++ thorus/fixture/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fixture
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = 5;
+            Assert.AreEqual(expected, 5);
+
+        }
+    }
+}
